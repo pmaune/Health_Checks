13b4c7f4642535281a13acc23ad01874d8633dbd
pmaune <37003235+pmaune@users.noreply.github.com>
2018-03-02 16:23:13 -0600

    Add files via upload

---
+++ Citrix Services Only.ps1
@@ -0,0 +1,7 @@
+﻿$Servers = @("ctrx-ddc5","ctrx-ddc6","ctrx-lic2","ctrx-sf5","ctrx-sf6","PVS3","PVS4","AzureMFA")
+
+forEach($Server in $Servers) {
+    Get-Service -ComputerName $Server |
+    ? {$_.starttype -like "*automatic*" -and $_.status -ne "Running" -and ($_.displayname -like "*citrix*" -or $_.displayname -like "*federation*")} |
+    select machinename,name,displayname,status,starttype | sort starttype | ft -AutoSize
+}
